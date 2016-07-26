d44b9c030af81cc9a8edc9a8eba8cd86b95f09f8
Monkey <371816216@qq.com>
2016-07-26 23:14:46 +0800

    add init file

---
+++ www/init/init.php
@@ -0,0 +1,8 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en",lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf8">
+	<title>Init Mysql</title>
+</head>
+<body>
+</body>
+</html>
