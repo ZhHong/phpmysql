e3685fe6ce5f09382ecfb87fb45125d1534085ea
Monkey <371816216@qq.com>
2016-07-27 00:12:30 +0800

    open sql file

--- www/init/init.php
+++ www/init/init.php
@@ -4,5 +4,30 @@
 	<title>Init Mysql</title>
 </head>
 <body>
+	<?php
+		$filename = "initproc.sql";
+		$file = fopen($filename,"r");
+		$create_sql_array = array();
+		$sql_line ="";
+		if ($file){
+			while (!feof($file)) {
+				$line = fgets($file);
+				//if not have // end the of the function
+				$has = stripos($line,"//");
+				if ($has === FALSE){
+					$sql_line = $sql_line.$line;
+				}else{
+					$create_sql_array[] = $sql_line;
+					$sql_line ="";
+				}
+			}
+		}else{
+			echo "open $filename failed !!";
+		}
+		
+		foreach($create_sql_array as $sql_state){
+			echo "<p>$sql_state </p>\n\n";
+		}
+	?>
 </body>
 </html>
