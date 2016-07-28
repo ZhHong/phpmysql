cef42b1320d7cf9e32d14aa01600ab613911ff04
Monkey <371816216@qq.com>
2016-07-28 23:28:30 +0800

    add login page

---
+++ www/index.php
@@ -0,0 +1,42 @@
+<?php 
+	$page_title = "Home";
+	include("includes/header.html")
+?>
+<h1>Count View</h1>
+		<p>We has X number of game data</p>
+		<table border="10">
+			<tr >
+				<th>Name</th>
+				<th>Number of items</th>
+				<th>Number of class</th>
+				<th>Number of members</th>
+			</tr>
+			<tr>
+				<td>a</td>
+				<td>a</td>
+				<td>a</td>
+				<td>a</td>
+			</tr>
+			<tr>
+				<td>a</td>
+				<td>a</td>
+				<td>a</td>
+				<td>a</td>
+			</tr>
+			<tr>
+				<td>a</td>
+				<td>a</td>
+				<td>a</td>
+				<td>a</td>
+			</tr>
+			<tr>
+				<td>a</td>
+				<td>a</td>
+				<td>a</td>
+				<td>a</td>
+			</tr>
+		</table>
+		<p>Every Game Data Info</p>
+<?php
+	include("includes/footer.html")
+?>
---
+++ www/login.php
@@ -0,0 +1,8 @@
+<?php 
+	$page_title = "Login";
+	include("includes/header.html")
+?>
+<h1>Login page</h1>
+<?php
+	include("includes/footer.html")
+?>
